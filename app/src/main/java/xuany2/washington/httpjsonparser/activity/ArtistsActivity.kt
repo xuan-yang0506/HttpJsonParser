c94b8e0cec88d5357299030d17de80f329dadf04
xuany2 <xuany2@uw.edu>
2020-05-09 14:45:32 -0700

    acvitivies layouts added

--- app/src/main/java/xuany2/washington/httpjsonparser/activity/ArtistsActivity.kt
+++ app/src/main/java/xuany2/washington/httpjsonparser/activity/ArtistsActivity.kt
@@ -1,4 +1,12 @@
 package xuany2.washington.httpjsonparser.activity
 
-class ArtistsActivity {
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import xuany2.washington.httpjsonparser.R
+
+class ArtistsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_artists)
+    }
 }
