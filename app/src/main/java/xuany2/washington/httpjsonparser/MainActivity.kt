556a0dda8676bf931f96e699bd98625767918514
xuany2 <xuany2@uw.edu>
2020-05-09 14:08:24 -0700

    Initial commit

---
+++ app/src/main/java/xuany2/washington/httpjsonparser/MainActivity.kt
@@ -0,0 +1,11 @@
+package xuany2.washington.httpjsonparser
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
